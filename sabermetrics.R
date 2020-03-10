5b0c83c8b51f3d5eb409b09a710e40e698931ac6
Yash Karandikar <yash@bungee-gum.attlocal.net>
2020-03-10 16:38:17 -0700

    change filter & stratify by year

--- sabermetrics.R
+++ sabermetrics.R
@@ -15,33 +15,31 @@ library(dplyr)
 # hr vs. rbi?
 
 data_baseball %>%
-  filter(year > 2010) %>%
-  ggplot(aes(x=bb, y=r, col=league_id, size=rbi)) + geom_point() + facet_wrap(~ibb) + labs(x='bases on balls', y='runs scored', title='Base on balls vs. runs scored', subtitle='stratified by intentional walks') + theme(plot.title = element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5))
+  filter(year >= 2010) %>%
+  ggplot(aes(x=bb, y=r, col=league_id, size=ibb)) + geom_point() + facet_wrap(~year) + labs(x='bases on balls', y='runs scored', title='Base on balls vs. runs scored', subtitle='stratified by intentional walks') + theme(plot.title = element_text(hjust=0.5))
   r_coef = cor(data_baseball$bb, data_baseball$r, use='complete.obs')
   print(r_coef)
 
 data_baseball %>%
-  filter(year > 2010) %>%
-  ggplot(aes(x=bb, y=so, col=league_id)) + geom_point() + labs(x='bases on balls', y='strikeouts', title='Base on balls vs. strikeouts') + theme(plot.title = element_text(hjust=0.5))
+  filter(year >= 2010) %>%
+  ggplot(aes(x=bb, y=so, col=league_id)) + geom_point() + facet_wrap(~year) + labs(x='bases on balls', y='strikeouts', title='Base on balls vs. strikeouts') + theme(plot.title = element_text(hjust=0.5))
   r_coef = cor(data_baseball$bb, data_baseball$so, use='complete.obs')
   print(r_coef)
 
 data_baseball %>%
-  filter(year > 2010) %>%
-  ggplot(aes(x=bb, y=rbi, col=league_id)) + geom_point() + labs(x='bases on balls', y='runs batted in', title='Base on balls vs. runs batted in') + theme(plot.title = element_text(hjust=0.5))
+  filter(year >= 2010) %>%
+  ggplot(aes(x=bb, y=rbi, col=league_id)) + geom_point() + facet_wrap(~year) + labs(x='bases on balls', y='runs batted in', title='Base on balls vs. runs batted in') + theme(plot.title = element_text(hjust=0.5))
   r_coef = cor(data_baseball$bb, data_baseball$rbi, use='complete.obs')
   print(r_coef) 
 
 data_baseball %>%
-  filter(year > 2010) %>%
-  ggplot(aes(x=bb, y=hr, col=league_id)) + geom_point() + labs(x='bases on balls', y='home runs', title='Base on balls vs. home runs') + theme(plot.title = element_text(hjust=0.5))
+  filter(year >= 2010) %>%
+  ggplot(aes(x=bb, y=hr, col=league_id)) + geom_point() + facet_wrap(~year) + labs(x='bases on balls', y='home runs', title='Base on balls vs. home runs') + theme(plot.title = element_text(hjust=0.5))
   r_coef = cor(data_baseball$bb, data_baseball$hr, use='complete.obs')
   print(r_coef)
 
 data_baseball %>%
-  filter(year > 2010) %>%
+  filter(year >= 2010) %>%
   ggplot(aes(x=hr, y=rbi, col=league_id, size=ab)) + geom_point() + facet_wrap(~ibb) + labs(x='home runs', y='runs batted in', title='Home runs vs. runs batted in', subtitle='stratified by intentional walks') + theme(plot.title = element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5))
   r_coef = cor(data_baseball$hr, data_baseball$rbi, use='complete.obs')
   print(r_coef)
-
-

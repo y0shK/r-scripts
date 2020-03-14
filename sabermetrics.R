94183fd44016a7be5c12b48f0b9466a7508682a0
Yash Karandikar <ynkarandikar@gmail.com>
2020-03-14 14:08:44 -0700

    remove hatteberg data, filter years once, add col and size args

--- sabermetrics.R
+++ sabermetrics.R
@@ -3,7 +3,7 @@
 getwd()
 setwd('/home/yash/hist_baseball')
 
-data_baseball_hitting <- read.csv('batting.csv')
+hitting_raw_data <- read.csv('batting.csv')
 
 library(ggplot2)
 library(dplyr)
@@ -14,56 +14,40 @@ library(dplyr)
 # base on balls vs. hr?
 # hr vs. rbi?
 
-#typeof(data_baseball_hitting) # list
-#typeof(data_baseball_hitting$r) # double
+#typeof(hitting_data) # list
+#typeof(hitting_data$r) # double
 
-data_baseball_hitting %>%
-  filter(year >= 2010) %>%
-  ggplot(aes(x=bb, y=r, col=league_id, size=ibb)) + geom_point() + facet_wrap(~year) + labs(x='bases on balls', y='runs scored', title='Base on balls vs. runs scored', subtitle='stratified by intentional walks') + theme(plot.title = element_text(hjust=0.5))
-  r_coef = cor(data_baseball_hitting$bb, data_baseball_hitting$r, use='complete.obs')
+hitting_data <- hitting_raw_data %>%
+  filter(year >= 2010)
+
+hitting_data %>%
+  ggplot(aes(x=bb, y=r, col=ibb, size=h)) + geom_point() + facet_wrap(~year) + labs(x='bases on balls', y='runs scored', title='Base on balls vs. runs scored', subtitle='stratified by intentional walks and hits') + theme(plot.title = element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5))
+  r_coef = cor(hitting_data$bb, hitting_data$r, use='complete.obs')
   print(r_coef)
 
-data_baseball_hitting %>%
-  filter(year >= 2010) %>%
-  ggplot(aes(x=bb, y=so, col=league_id)) + geom_point() + facet_wrap(~year) + labs(x='bases on balls', y='strikeouts', title='Base on balls vs. strikeouts') + theme(plot.title = element_text(hjust=0.5))
-  r_coef = cor(data_baseball_hitting$bb, data_baseball_hitting$so, use='complete.obs')
+hitting_data %>%
+  ggplot(aes(x=bb, y=so, col=ibb, size=h)) + geom_point() + facet_wrap(~year) + labs(x='bases on balls', y='strikeouts', title='Base on balls vs. strikeouts', subtitle='stratified by intentional walks and hits') + theme(plot.title = element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5))
+  r_coef = cor(hitting_data$bb, hitting_data$so, use='complete.obs')
   print(r_coef)
 
-data_baseball_hitting %>%
-  filter(year >= 2010) %>%
-  ggplot(aes(x=bb, y=rbi, col=league_id)) + geom_point() + facet_wrap(~year) + labs(x='bases on balls', y='runs batted in', title='Base on balls vs. runs batted in') + theme(plot.title = element_text(hjust=0.5))
-  r_coef = cor(data_baseball_hitting$bb, data_baseball_hitting$rbi, use='complete.obs')
+hitting_data %>%
+  ggplot(aes(x=bb, y=rbi, col=ibb, size=hr)) + geom_point() + facet_wrap(~year) + labs(x='bases on balls', y='runs batted in', title='Base on balls vs. runs batted in', subtitle='stratified by intentional walks and home runs') + theme(plot.title = element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5))
+  r_coef = cor(hitting_data$bb, hitting_data$rbi, use='complete.obs')
   print(r_coef) 
 
-data_baseball_hitting %>%
-  filter(year >= 2010) %>%
-  ggplot(aes(x=bb, y=hr, col=league_id)) + geom_point() + facet_wrap(~year) + labs(x='bases on balls', y='home runs', title='Base on balls vs. home runs') + theme(plot.title = element_text(hjust=0.5))
-  r_coef = cor(data_baseball_hitting$bb, data_baseball_hitting$hr, use='complete.obs')
+hitting_data %>%
+  ggplot(aes(x=bb, y=hr, col=ibb, size=rbi)) + geom_point() + facet_wrap(~year) + labs(x='bases on balls', y='home runs', title='Base on balls vs. home runs', subtitle='stratified by intentional walks and RBI') + theme(plot.title = element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5))
+  r_coef = cor(hitting_data$bb, hitting_data$hr, use='complete.obs')
   print(r_coef)
 
-data_baseball_hitting %>%
-  filter(year >= 2010) %>%
-  ggplot(aes(x=hr, y=rbi, col=league_id, size=ab)) + geom_point() + facet_wrap(~ibb) + labs(x='home runs', y='runs batted in', title='Home runs vs. runs batted in', subtitle='stratified by intentional walks') + theme(plot.title = element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5))
-  r_coef = cor(data_baseball_hitting$hr, data_baseball_hitting$rbi, use='complete.obs')
+hitting_data %>%
+  ggplot(aes(x=hr, y=rbi, col=ibb, size=bb)) + geom_point() + facet_wrap(~year) + labs(x='home runs', y='runs batted in', title='Home runs vs. runs batted in', subtitle='stratified by intentional walks and walks') + theme(plot.title = element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5))
+  r_coef = cor(hitting_data$hr, hitting_data$rbi, use='complete.obs')
   print(r_coef)
 
 # on-base percentage vs. runs scored  
-data_baseball_hitting %>%
-  filter(year >= 2010) %>%
-  ggplot(aes(x=(h + bb + hbp)/(ab+bb+hbp+sf), y=r, col=league_id, size=hr)) + geom_point() + facet_wrap(~year) + labs(x='OBP', y='runs scored', title='OBP vs. runs scored', subtitle='shaped by home run count') + theme(plot.title = element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5))
+hitting_data %>%
+  ggplot(aes(x=(h + bb + hbp)/(ab+bb+hbp+sf), y=r, col=hr, size=bb)) + geom_point() + facet_wrap(~year) + labs(x='OBP', y='runs scored', title='OBP vs. runs scored', subtitle='shaped by walks and home run count') + theme(plot.title = element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5))
 
 # slugging precentage vs. runs scored
-data_baseball_hitting_filter <- data_baseball_hitting %>%
-  filter(year >= 2010)
-
-ggplot(data=data_baseball_hitting_filter, aes(x=(h + 2*double + 3*triple+4*hr)/ab, y=r, color=hr, size=bb)) + geom_point() + labs(x='SLG', y='runs scored', title='SLG vs. runs scored', subtitle='shaped by walk count') + theme(plot.title = element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5)) + scale_x_continuous(limits=c(0, 1))
-
-# analyze player data of Scott Hatteberg, a Moneyball-era catcher for the Oakland A's
-# how did his OBP affect his runs scored? how about his OPS?
-hitter_data <- data_baseball_hitting %>%
-  select(player_id, year, bb, r, h, hbp, ab, sf, h, double, triple, hr) %>%
-  filter(player_id == 'hattesc01') %>%
-  group_by(year) %>%
-  summarise(at_bats = ab, hits = h, walks = bb, OBP = (h+bb+hbp)/(ab+bb+hbp+sf), SLG = (h + 2*double + 3*triple+4*hr)/(ab), OPS = OBP+SLG, runs_scored = r)
-
-View(player_data)
+ggplot(data=hitting_data, aes(x=(h + 2*double + 3*triple+4*hr)/ab, y=r, color=hr, size=bb)) + geom_point()+ facet_wrap(~year) + labs(x='SLG', y='runs scored', title='SLG vs. runs scored', subtitle='shaped by walk count and home runs') + theme(plot.title = element_text(hjust=0.5), plot.subtitle = element_text(hjust=0.5)) + scale_x_continuous(limits=c(0, 1))

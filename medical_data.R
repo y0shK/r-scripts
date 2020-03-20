6dadfa29ac1578d9e372924869e774bc612ceae3
Yash Karandikar <ynkarandikar@gmail.com>
2020-03-19 17:48:00 -0700

    scatterplot3d + lattice graphs w/ densityplot

--- medical_data.R
+++ medical_data.R
@@ -18,3 +18,21 @@ data %>%
 data %>%
   ggplot(aes(x=smoothness_mean, y=compactness_mean, col=diagnosis, size=area_mean)) + geom_point() +
   ggtitle('Smoothness vs. Compactness') + theme(plot.title = element_text(hjust=0.5))
+
+# from https://www.statmethods.net/advgraphs/trellis.html
+# and http://www.sthda.com/english/wiki/scatterplot3d-3d-graphics-r-software-and-data-visualization
+
+library(scatterplot3d)
+attach(data)
+
+scatterplot3d(x=texture_mean, y=compactness_mean, z=diagnosis, color='darkred', pch=16, box=FALSE)
+scatterplot3d(x=texture_mean, y=smoothness_mean, z=diagnosis, color='steelblue', pch=16, box=FALSE)
+scatterplot3d(x=smoothness_mean, y=compactness_mean, z=diagnosis, color='darkgreen', pch=16, box=FALSE)
+
+library(lattice)
+
+# plots the frequency of mean texture
+densityplot(~texture_mean, main='Mean texture density plot', xlab='Mean texture')
+
+radius.f <- factor(radius_mean, levels=c(quantile(radius_mean, 0.25), quantile(radius_mean, 0.50), quantile(radius_mean, 0.75)))
+densityplot(~smoothness_mean | radius.f, main = 'Mean smoothness by mean radius', xlab='Mean smoothness', layout=c(1,3))

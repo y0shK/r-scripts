f558aa0cc00ac924eb49053c9c6301e58a62e08b
Yash Karandikar <ynkarandikar@gmail.com>
2020-03-20 14:39:25 -0700

    e1071 density plot, linear regression + stat tests

--- public_health.R
+++ public_health.R
@@ -29,6 +29,7 @@ attach(world_data_2015)
 # broom + lm() with ggplot: https://stackoverflow.com/questions/36731027/how-can-i-plot-the-residuals-of-lm-with-ggplot
 
 library(broom)
+
 gdp_health <- lm(Health..Life.Expectancy. ~ Economy..GDP.per.Capita.)
 
 ggplot(gdp_health, aes(x=.fitted, y=.resid)) + geom_point() + labs(x='GDP per capita', y='Residuals', title='GDP per capita vs. Health, residuals')
@@ -38,3 +39,35 @@ library(scatterplot3d)
 
 densityplot(Economy..GDP.per.Capita., main='Economy: GDP per capita')
 scatterplot3d(x=Economy..GDP.per.Capita., y=Freedom, z=Health..Life.Expectancy., pch=16, color='steelblue', box=FALSE)
+
+# density plot with library e1071
+
+library(e1071)
+
+plot(density(Economy..GDP.per.Capita.), main='Density plot: GDP per capita', ylab='Frequency')
+polygon(density(Economy..GDP.per.Capita.), col='blue')
+
+plot(density(Health..Life.Expectancy.), main='Density plot: Health and life expectancy', ylab='Frequency')
+polygon(density(Health..Life.Expectancy.), col='green')
+
+# linear regression for world_data_2015
+# from http://r-statistics.co/Linear-Regression.html
+lin_reg <- lm(Health..Life.Expectancy. ~ Economy..GDP.per.Capita., data=world_data_2015)
+tidy(lin_reg) # use function tidy because it presents the data like a data frame
+
+model_summary <- summary(lin_reg)
+print(model_summary)
+
+model_coefficients <- model_summary$coefficients
+
+beta.estimate <- model_coefficients['Economy..GDP.per.Capita.', 'Estimate'] # find the likelihood of type 2 error
+std.error <- model_coefficients['Economy..GDP.per.Capita.', 'Std. Error'] # standard error
+
+# function pt finds the p-value from the given data
+t_val <- beta.estimate / std.error # t = beta / sd
+p_val <- 2 * pt(-abs(t_val), df=nrow(world_data_2015)-ncol(world_data_2015))
+
+# print string and variable contents successively
+# https://stackoverflow.com/questions/15589601/print-string-and-variable-contents-on-the-same-line-in-r
+print(paste0('t-value: ', t_val))
+print(paste0('p-value: ', p_val))

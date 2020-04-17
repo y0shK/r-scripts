58efb9ae64da011c48a59aa66c90f14c76fa8158
Yash Karandikar <ynkarandikar@gmail.com>
2020-04-17 10:58:33 -0700

    fasteR ch 4 - R factors

--- fasteR.R
+++ fasteR.R
@@ -38,9 +38,11 @@ sum(mtcars$hp > 100) # yields 23, i.e. 23 years
 # has multiple individuals with multiple pieces of information each
 # e.g. age, hair color, likes sports? Y/N
 
-age_vector_df <- c(23, 34, 10, 92, 54, 22, 12, 52, 56)
-hair_color_vector_df <- c('black', 'black', 'red', 'blue', 'brown', 'blond', 'blond', 'red', 'black')
+age_vector_df <- c(23, 34, 10, 92, 54, 22, 12, 52, 56) # numeric vector
+hair_color_vector_df <- as.factor(c('black', 'black', 'red', 'blue', 'blond', 'blond', 'blond', 'red', 'black')) # converted to factor - can be used to block
 likes_sports_vector_df <- as.logical(c(0, 0, 0, 1, 0, 1, 1, 0, 1)) # converts 0/1 to false/true
+
+levels(hair_color_vector_df) # all possible versions of factor
 
 example_data_df <- data.frame(age_vector_df, hair_color_vector_df, likes_sports_vector_df)
 names(example_data_df) <- c('Age', 'Hair color', 'Likes sports')
@@ -54,3 +56,29 @@ example_data_df[2:5, c(1, 3)] # from rows 2-5, pull columns 1 and 3
 
 tg <- ToothGrowth
 which(tg$len < 16) # tooth length < 16
+
+# factor class is used for categorical variables
+  # similar to blocking in experimental design
+class(hair_color_vector_df) # character, though used as a factor class - converted to factor with as.factor coercion
+
+class(tg) # data.frame, appends multiple varaibles of multiple subjects
+class(tg$supp) # factor, acts as a blocking variable to analyze dose
+
+# how likely is it to like sports given hair color?
+black_hair <- example_data_df[example_data_df$`Hair color` == 'black', ] # produce boolean vector, take rows of 'black' and all columns
+  # type of black_hair -> data.frame
+
+blond_hair <- example_data_df[example_data_df$`Hair color` == 'blond', ]
+
+determine_sports_black_hair_subset <- black_hair[black_hair$`Likes sports` == TRUE, ]
+determine_sports_blond_hair_subset <- blond_hair[blond_hair$`Likes sports` == TRUE, ]
+
+nrow(determine_sports_black_hair_subset) # how many people like sports, i.e. == TRUE?
+nrow(determine_sports_blond_hair_subset)
+
+# test how mean ages compare to hair (no real correlation)
+determine_age_black_hair_subset <- mean(black_hair$Age)
+determine_age_blond_hair_subset <- mean(blond_hair$Age)
+
+determine_age_black_hair_subset
+determine_age_blond_hair_subset
